3660d7e88ff8ae02ae228888278e283741b5be28
Jordi Conde Molina <62895555+FINNO51@users.noreply.github.com>
2024-05-20 19:39:05 +0200

    60fps cap

--- MyScene.js
+++ MyScene.js
@@ -797,7 +797,9 @@ class MyScene extends THREE.Scene {
     // Este método debe ser llamado cada vez que queramos visualizar la escena de nuevo.
     // Literalmente le decimos al navegador: "La próxima vez que haya que refrescar la pantalla, llama al método que te indico".
     // Si no existiera esta línea,  update()  se ejecutaría solo la primera vez.
-    requestAnimationFrame(() => this.update())
+    setTimeout(function(){ 
+    requestAnimationFrame(() => this.update());
+   },1000/60);
   }
 
   movimiento(){
